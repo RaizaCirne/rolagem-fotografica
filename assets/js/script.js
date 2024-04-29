4bde4e65038c4b16191e01bae2c78b923d11d19e
Raiza Cirne <raizacirne@gmail.com>
2024-04-28 22:35:09 -0300

    Style: Change scroll time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-var intervalo = 2000; // Intervalo de 2 segundos
+var intervalo = 1500;
 var imagens = [];
 
 for (var i = 1; i <= 14; i++) {
-  imagens.push('/assets/images/' + (i < 10 ? '0' : '') + i + '.jpg');
+  imagens.push("/assets/images/" + (i < 10 ? "0" : "") + i + ".jpg");
 }
 
 var contador = 0;
 
 function trocarImagem() {
-  document.getElementById('music-img').src = imagens[contador];
+  document.getElementById("music-img").src = imagens[contador];
   contador = (contador + 1) % imagens.length; // Avança para a próxima imagem ou volta ao início do array se atingir o final
   setTimeout(trocarImagem, intervalo);
 }
